847cebb47ac04536c6a15ce2ec419cbf61c9f51c
Efuathesingingdeveloper <66095416+Efuathesingingdeveloper@users.noreply.github.com>
2020-10-14 13:30:07 -0400

    used resources for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,29 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create,]
-   get 'login', to: 'sessions#new'
-   post 'login', to: 'sessions#create'
-   #index
-get '/songs', to: 'songs#index'
-#new
-get 'songs/new', to: 'songs#new'
-#show
-get '/songs/:id', to: 'songs#show', as: 'song'
-#create
-post '/songs', to:"song#create"
-#edit
-get '/songs/:id/edit', to:'songs#edit', as "edit_song"
-#update
-patch '/songs/:id/edit', to: 'songs#update'
-#destroy
-delete '/songs/:id', to: 'song#destroy'
+  resources :songs, except: [:update]
+#    get 'login', to: 'sessions#new'
+#    post 'login', to: 'sessions#create'
+#    #index
+# get '/songs', to: 'songs#index'
+# #new
+# get 'songs/new', to: 'songs#new'
+# #show
+# get '/songs/:id', to: 'songs#show', as: 'song'
+# #create
+# post '/songs', to:"song#create"
+# #edit
+# get '/songs/:id/edit', to:'songs#edit', as "edit_song"
+# #update
+# patch '/songs/:id/edit', to: 'songs#update'
+# #destroy
+# delete '/songs/:id', to: 'songs#destroy'
 
-   post '/users', to: 'users#create'
+#    post '/users', to: 'users#create'
 
-   get 'welcome', to: 'sessions#welcome'
+#    get 'welcome', to: 'sessions#welcome'
 
-   get 'authorized', to: 'sessions#page_requires_login'
+#    get 'authorized', to: 'sessions#page_requires_login'
 
-   delete '/logout', to:'sessions#destroy'
+#    delete '/logout', to:'sessions#destroy'
 
    get '/auth/facebook/callback' => 'sessions#create'
 end
