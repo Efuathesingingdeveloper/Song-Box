40d6af35249bf03f64b8564da46d187b367587d7
Efuathesingingdeveloper <66095416+Efuathesingingdeveloper@users.noreply.github.com>
2020-10-16 14:48:37 -0400

    refractored lyrics controller  + helper methods

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -1,26 +1,29 @@
 class LyricsController < ApplicationController
   before_action :require_login
-  before_action :validate_song
+  before_action :get_song, only: [:index, :new]
 
 
   def index
-    get_song
-    
-    end
+    redirect_if_request_invalid
+  
   end 
+
     def new
-       get_song
+          redirect_if_request_invalid
        @lyric = Lyric.new
     end
   
     def create
         @song = Song.find_by(id: params[:lyric][:song_id])
+        if user_authorized?
         @lyric = Lyric.new(lyric_params)
       if @lyric.save
         redirect_to song_lyrics_path(@lyric.song)
       else
         render :new
       end
+    else
+      redirect_to songs_path
     end
 
   
@@ -41,13 +44,20 @@ class LyricsController < ApplicationController
 
 
  private
+
 def get_song
-    @song = Song.find_by(id: params[:song_id])
+    @song || = Song.find_by(id: params[:song_id])
 end 
 
-def validate_song
-  redirect_to songs_path  unless get_song 
+def user_authorized?
+ @song.user && (@song.user.id == current_user.id)
 end 
+
+def redirect_if_request_invalid
+  if @song.nil? || !user_authorized?
+    redirect_to songs_path
+  end 
+end
 
 def lyric_params
     params.require(:lyric).permit(:part_id, :count, :genre, :song_id)
