792947b08ff35e0081a1c441489d09ecc3465a51
Efuathesingingdeveloper <66095416+Efuathesingingdeveloper@users.noreply.github.com>
2020-10-16 09:02:58 -0400

    built out lyrics controller and forms

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -1,18 +1,35 @@
 class LyricsController < ApplicationController
 
     def index
-        @song = Song.find_by(id: params[:song_id])
+      get_songs
     end
   
   
     def new
-     
+       get_songs
+       redirect_to songs_path if !@song
+       @lyric = Lyric.new
     end
   
     def create
-      
+        @song= Song.find_by(id: params[:lyric][:song_id])
+        @lyric = Lyric.new(lyric_params)
+      if @lyric.save
+        redirect_to song_lyrics_path(@lyric.song)
+      else
+        render :new
+      end
     end
   
     def destroy
       
     end
+
+    private
+def get_songs
+    @song = Song.find_by(id: params[:song_id])
+end 
+
+def lyric_params
+    params.require(:lyric).permit(:part_id, :count, :genre, :song_id)
+end
